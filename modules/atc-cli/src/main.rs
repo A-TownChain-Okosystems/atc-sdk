da3caa50ef3db69dc8652615671c6ba135cbc29a
ShivaCoreDev <michael.worob@gmail.com>
2026-09-16 12:32:40 +0200

    fix(cli): remove unused import from bootstrap

--- modules/atc-cli/src/main.rs
+++ modules/atc-cli/src/main.rs
@@ -1,8 +1,6 @@
 // Copyright (c) 2026 Michael Wroblewski / ShivaCore / A-TownChain-Okosystems. All Rights Reserved.
 use std::env;
 
-use atc_cli::*;
-
 fn print_help() {
     println!("atc-cli — A-TownChain-Okosystems");
     println!();
